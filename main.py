5fbd6e6aa363b4f4bda95c52d8c0d7b41d023684
dsledge1 <david.sledge1@gmail.com>
2025-03-05 11:34:50 -0500

    Added sys.argv functionality

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from stats import *
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")     
+    sys.exit(1)
 
 def main():
     #count=wordcount("./books/frankenstein.txt")
@@ -6,11 +10,11 @@ def main():
     #print(cc("./books/frankenstein.txt"))
     #sorted=dict_sorter("./books/frankenstein.txt")
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
-    print(f"Found {wordcount("./books/frankenstein.txt")} total words")
+    print(f"Found {wordcount(sys.argv[1])} total words")
     print("--------- Character Count -------")
-    for dict in dict_sorter("./books/frankenstein.txt"):
+    for dict in dict_sorter(sys.argv[1]):
         if dict['char'].isalpha() == True:
                 print(f"{dict['char']}: {dict['count']}")
 
